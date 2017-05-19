e6bd8f06454f2c81ba07292e7adccfbe8691339f
Shibao <marcio.shibao@caelum.com.br>
2017-05-19 14:01:37 -0300

    Details da nota fiscal, adicionando items com ViewScoped

--- src/main/java/br/com/caelum/notasfiscais/mb/NotaFiscalBean.java
+++ src/main/java/br/com/caelum/notasfiscais/mb/NotaFiscalBean.java
@@ -1,26 +1,65 @@
 package br.com.caelum.notasfiscais.mb;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.caelum.notasfiscais.dao.NotaFiscalDao;
+import br.com.caelum.notasfiscais.dao.ProdutoDao;
+import br.com.caelum.notasfiscais.modelo.Item;
 import br.com.caelum.notasfiscais.modelo.NotaFiscal;
+import br.com.caelum.notasfiscais.modelo.Produto;
 
 @Named
-@RequestScoped
-public class NotaFiscalBean {
+@ViewScoped
+public class NotaFiscalBean implements Serializable{
 
 	private NotaFiscal notaFiscal = new NotaFiscal();
 	@Inject
 	private NotaFiscalDao nDao;
+	@Inject
+	private ProdutoDao pDao;
+	private Item item = new Item();
+	private Long idProduto;
 	
 	public NotaFiscal getNotaFiscal() {
 		return notaFiscal;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	
+	public Long getIdProduto() {
+		return idProduto;
+	}
+	
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+	
+	public void guardaItem(){
+		Produto produto = pDao.buscaPorId(idProduto);
+		item.setProduto(produto);
+		item.setValorUnitario(produto.getPreco());
+		
+		item.setNotaFiscal(notaFiscal);
+		notaFiscal.getItens().add(item);
+		
+		item = new Item();
+		setIdProduto(null);
 	}
 	
 	public void gravar(){
 		nDao.adiciona(notaFiscal);
 		this.notaFiscal = new NotaFiscal();
 	}
+	
+	
 }
