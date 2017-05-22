1d2fbccc82bdf549b463db353485ecc6d49aca5a
Shibao <marcio.shibao@caelum.com.br>
2017-05-22 12:41:50 -0300

    Criando os emails com qualifier

--- src/main/java/br/com/caelum/notasfiscais/listener/LoginListener.java
+++ src/main/java/br/com/caelum/notasfiscais/listener/LoginListener.java
@@ -1,13 +1,24 @@
 package br.com.caelum.notasfiscais.listener;
 
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 
 import br.com.caelum.notasfiscais.modelo.Usuario;
+import br.com.caelum.notasfiscais.util.EmailComercial;
+import br.com.caelum.notasfiscais.util.EmailFinanceiro;
 
 public class LoginListener {
 
+	@Inject
+	@EmailComercial
+	private String emailComercial;
+
+	@Inject
+	@EmailFinanceiro
+	private String emailFinanceiro;
+	
 	public void onLogin(@Observes Usuario usuario) {
 		System.out.printf("Usuario %s se logou no sistema. ", usuario.getLogin());
-		// enviando email para outro sistema.
+		System.out.println("Enviando emai para " + emailComercial + " e para " + emailFinanceiro);
 	}
 }
